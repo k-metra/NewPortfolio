87c54af9207b047835a09dd6eef1204abc443e1c
k-metra <metrakurt@gmail.com>
2025-10-30 19:17:41 +0800

    feat: add Home component with HeroCard integration

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import HeroCard from "../components/heroCard";
+
+export default function Home() {
+    return (
+        <main className="min-w-screen min-h-screen flex flex-col items-center pt-20 pb-10 bg-background">
+            <HeroCard />
+        </main>
+    )
+}
