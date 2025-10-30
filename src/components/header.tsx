7067ac5cb4942f65d40c7ca4dd26ca2431f97612
k-metra <metrakurt@gmail.com>
2025-10-30 18:00:25 +0800

    feat: implement animations to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,32 @@
+import { motion } from "framer-motion";
+
 export default function Header() {
     return (
-        <header className="font-jetbrains w-screen min-h-16 bg-background text-text-primary flex items-center justify-between px-28">
-            <h1 className="text-2xl font-bold">Kurt Metra</h1>
+        <header className="font-jetbrains w-screen min-h-16 bg-background cursor-pointer text-text-primary flex items-center justify-between px-28">
+            <motion.h1 
+            initial={{ opacity: 0, y: -25 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            whileHover = {{
+                scale: 1.05,
+                transition: {
+                    duration: 0.2,
+                    ease: "easeOut",
+                }
+            }}
+            className="text-2xl font-bold">
+                Kurt Metra
+            </motion.h1>
 
-            <div className="flex flex-row space-x-6 gap-1">
+            <motion.div 
+            initial={{ opacity: 0, y: -25 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="flex flex-row space-x-6 gap-1">
                 <a href="#about" className="hover:text-gray-400">About</a>
                 <a href="#projects" className="hover:text-gray-400">Projects</a>
                 <a href="#contact" className="hover:text-gray-400">Contact</a>
-            </div>
+            </motion.div>
         </header>
     )
 }
