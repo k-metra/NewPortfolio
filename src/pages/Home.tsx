4786e4445fb85ee53a63c00424667d06b4ff3987
k-metra <metrakurt@gmail.com>
2025-10-31 06:52:40 +0800

    feat: add Footer component to Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import Experience from "../components/experience";
+import Footer from "../components/footer";
 import HeroCard from "../components/heroCard";
 import TechStack from "../components/techStack";
 
 export default function Home() {
     return (
-        <main className="w-full min-h-screen flex flex-col items-center pt-20 pb-10 bg-background px-4 overflow-x-hidden">
+        <main className="w-full min-h-screen flex flex-col items-center pt-20 bg-background px-4 overflow-x-hidden">
             <HeroCard />
             <TechStack />
             <Experience />
+            <Footer/>
         </main>
     )
 }
