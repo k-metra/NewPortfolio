a8417c58a9665cae40582ced7093c6b06c9d59d1
k-metra <metrakurt@gmail.com>
2025-10-31 20:53:52 +0800

    feat: add Kofi component for donations support

---
+++ src/components/kofi.tsx
@@ -0,0 +1,10 @@
+export default function Kofi() {
+    return (
+        <div className="my-10 flex flex-col justify-center items-center">
+            <h1 className="text-center block font-jetbrains text-4xl text-text-primary my-6">Wanna support me?</h1>
+            <iframe id='kofiframe' className="border-none w-full p-1 bg-[#f9f9f9]" src='https://ko-fi.com/kurtmetra/?hidefeed=true&widget=true&embed=true&preview=true'  height='712' title='kurtmetra'></iframe>
+
+
+        </div>
+    )
+}
